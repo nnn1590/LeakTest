b3dfac84ff8f8bf3f0c890704b4634f16eae5c24
NNN1590 <nnn1590@nnn1590.org>
2022-06-12 04:34:49 +0900

    Flags.cs: Add TestType.TestStruct1 and {TestType,RunType}.None
    And remove unused 'using' directives

--- Flags.cs
+++ Flags.cs
@@ -1,17 +1,17 @@
 namespace NNN1590.LeakTest;
 
-using System.Runtime.InteropServices;
-using static Native;
-
 public static class Flags {
 	[Flags]
 	public enum TestType {
+		None = 0x0,
 		ReturnCharPtrFunc = 0x1,
 		ReturnIntFunc = 0x2,
-		ReturnIntPtrFunc = 0x4
+		ReturnIntPtrFunc = 0x4,
+		TestStruct1 = 0x8
 	}
 	[Flags]
 	public enum RunType {
+		None = 0x0,
 		LeakingAndNotLeaking = 0x1,
 		NotLeakingAndLeaking = 0x2,
 		LeakingOnly = 0x4,
