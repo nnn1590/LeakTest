b09c369bf3f39b7c285ccd7450692c48e833cf0c
NNN1590 <nnn1590@nnn1590.org>
2022-06-12 04:30:48 +0900

    native/: Add TestStruct1 and testStruct()
    And:
    	- Makefile: Add dependency libleaktest.h and Makefile to 'libleaktest.so' rule
    	- Makefile: Add 'all' rule
    	- Flush stdout/stderr after (f)printf

--- native/libleaktest.c
+++ native/libleaktest.c
@@ -4,26 +4,47 @@
 void testCharPtr(ReturnCharPtrFunc func) {
 	if (func == NULL) {
 		fprintf(stderr, "Error: ReturnCharPtrFunc is null\n");
+		fflush(stderr);
 		return;
 	}
 	printf("ReturnCharPtrFunc: %s\n", func());
+	fflush(stdout);
 }
 
 void testInt(ReturnIntFunc func) {
 	if (func == NULL) {
 		fprintf(stderr, "Error: ReturnIntFunc is null\n");
+		fflush(stderr);
 		return;
 	}
 	printf("ReturnIntFunc: %d\n", func());
+	fflush(stdout);
 }
 
 void testIntPtr(ReturnIntPtrFunc func) {
 	if (func == NULL) {
 		fprintf(stderr, "Error: ReturnIntPtrFunc is null\n");
+		fflush(stderr);
 		return;
 	}
 	int* result = func();
 	printf("ReturnIntFunc: ");
 	if (result == NULL) printf("(null)\n");
 	else printf("%d\n", *result);
+	fflush(stdout);
+}
+
+TestStruct1 testStruct(TestStruct1 testStruct1) {
+	printf("1: %s\n", testStruct1.str1);
+	printf("2: %s\n", testStruct1.str2);
+	printf("3: %s\n", testStruct1.str3);
+	printf("4: %s\n", testStruct1.str4);
+	printf("5: %s\n", testStruct1.str5);
+	printf("6: %s\n", testStruct1.str6);
+	printf("7: %s\n", testStruct1.str7);
+	printf("8: %s\n", testStruct1.str8);
+	printf("9: %s\n", testStruct1.str9);
+	printf("10: %s\n", testStruct1.str10);
+	fflush(stdout);
+	return testStruct1;
 }
--- native/libleaktest.h
+++ native/libleaktest.h
@@ -2,9 +2,24 @@
 extern "C" {
 #endif
 
-typedef int (*ReturnIntFunc)(void);
-typedef int* (*ReturnIntPtrFunc)(void);
+typedef int   (*ReturnIntFunc)(void);
+typedef int*  (*ReturnIntPtrFunc)(void);
 typedef char* (*ReturnCharPtrFunc)(void);
+
+typedef struct {
+	char* str1;
+	char* str2;
+	char* str3;
+	char* str4;
+	char* str5;
+	char* str6;
+	char* str7;
+	char* str8;
+	char* str9;
+	char* str10;
+} TestStruct1;
+
+TestStruct1 testStruct(TestStruct1 testStruct1);
 
 #ifdef __cplusplus
 }
